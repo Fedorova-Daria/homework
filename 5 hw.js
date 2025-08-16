4dc0bcc3decb58bbe779d0b0e8e9512c07e93548
FedorovaDaria <fedorovada06@gmail.com>
2025-08-16 17:49:29 +0500

    New OOP

--- 5 hw.js
+++ 5 hw.js
@@ -9,31 +9,57 @@ character.prototype.say = function () {
 };
 
 function Orc(name, language, weapon) {
-  Character.call(this, 'Орк', name, language);
+  character.call(this, 'Орк', name, language);
   this.weapon = weapon;
 }
 
 // Orc.prototype теперь наследует методы от Character.prototype
-Orc.prototype = Object.create(Character.prototype);
+Orc.prototype = Object.create(character.prototype);
 
 Orc.prototype.attack = function () {
   console.log('АТАКА!');
 };
 
-Orc.prototype.castSpell = function (spellName) {
-  console.log(
-    `${this.name} (${this.race}) применяет заклинание "${spellName}"!`
-  );
+function Elf(name, language) {
+  character.call(this, 'Эльф', name, language);
+  this.spells = new Map();
+}
+
+Elf.prototype = Object.create(Character.prototype);
+Elf.prototype.constructor = Elf;
+
+Elf.prototype.createSpell = function (spellName, effect) {
+  if (this.spells.has(spellName)) {
+    console.log(
+      `${this.name} (${this.race}): Заклинание "${spellName}" уже существует.`
+    );
+  } else {
+    this.spells.set(spellName, effect);
+    console.log(
+      `${this.name} (${this.race}) создает новое заклинание: "${spellName}" с эффектом: "${effect}".`
+    );
+  }
 };
 
-Orc.prototype.createSpell = function (spellName, effect) {
-  console.log(
-    `${this.name} (${this.race}) создает новое заклинание: "${spellName}" с эффектом: "${effect}".`
-  );
+Elf.prototype.castSpell = function (spellName) {
+  if (this.spells.has(spellName)) {
+    const effect = this.spells.get(spellName);
+    console.log(
+      `${this.name} (${this.race}) применяет заклинание "${spellName}"! Эффект: "${effect}".`
+    );
+  } else {
+    console.log(
+      `${this.name} (${this.race}): Заклинание "${spellName}" не найдено в книге заклинаний.`
+    );
+  }
 };
 
 const orc = new character('Орк', 'Горг', 'Оркский');
 orc.say();
 orc.attack();
-orc.castSpell('Пламя Хаоса');
-orc.createSpell('Портал', 'Создает портал в Оргриммар');
+
+const elrond = new Elf('Элронд', 'Высокоэльфийский');
+elrond.say();
+elrond.createSpell('Огненный шар', 'Вызывает взрыв огня');
+elrond.createSpell('Ледяная стрела', 'Замораживает противника');
+elrond.castSpell('Огненный шар');
